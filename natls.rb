b731c478cf6ab0b7e6dc9962640b10303932bb02
Will Lane <williamlane923@gmail.com>
2020-11-24 13:34:39 -0800

    first commit

---
+++ natls.rb
@@ -0,0 +1,18 @@
+class Natls < Formula
+  desc "⚡️ nat - the 'ls' replacement you never knew you needed⚡️"
+  homepage "https://github.com/willdoescode/nat"
+  url "https://github.com/willdoescode/nat/archive/v2.1.8.tar.gz"
+  sha256 "cf71d0bfab2a2a0c3ee091c99044c9b177ad0162365af86eb62596648cd2b3f1"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/natls", "-v"
+  end
+end
+
