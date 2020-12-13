bf127696a30011b2939a5e04ec096aa8470aef12
Will Lane <williamlane923@gmail.com>
2020-12-13 11:18:18 -0800

    updated nat version number

--- natls.rb
+++ natls.rb
@@ -1,7 +1,7 @@
 class Natls < Formula
   desc "⚡️ nat - the 'ls' replacement you never knew you needed⚡️"
   homepage "https://github.com/willdoescode/nat"
-  url "https://github.com/willdoescode/nat/archive/v2.1.8.tar.gz"
+  url "https://github.com/willdoescode/nat/archive/v2.1.9.tar.gz"
   sha256 "cf71d0bfab2a2a0c3ee091c99044c9b177ad0162365af86eb62596648cd2b3f1"
   license "MIT"
 
